30848de490586981fbee91d82f5239c308e83802
Domenico De Blasi <domenico.deblasi98@gmail.com>
2022-07-12 19:30:51 +0200

    fixed hover pseudo-class for touch screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 const dropdownLinks = document.querySelectorAll(".link");
 const hamburger = document.querySelector("#hamburger");
 
+// close hamburger menu
 for (let link of dropdownLinks) {
     link.addEventListener("click", closeHamburger);
 }
